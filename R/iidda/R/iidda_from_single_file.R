59212e3e8f236895656b6832d9ee3061918bb4f1
stevencarlislewalker <steve.walker@utoronto.ca>
2021-07-23 13:11:39 -0400

    fix classic badge

--- R/iidda/R/iidda_from_single_file.R
+++ R/iidda/R/iidda_from_single_file.R
@@ -76,7 +76,7 @@ initialize_iidda_readme = function(
 
   classic_badge = ifelse(
     classic_iidda_source,
-    "[![Classic IIDDA](https://img.shields.io/badge/Lifecycle-Experimental-339999)](https://davidearn.mcmaster.ca/iidda)",
+    "[![Classic IIDDA badge](https://img.shields.io/static/v1.svg?label=Classic&message=IIDDA&color=blue)](https://davidearn.mcmaster.ca/iidda)",
     "")
 
   template = "
