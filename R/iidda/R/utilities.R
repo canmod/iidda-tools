e1cf1f8add8398e17f6629fbe7de1508dabaa384
stevencarlislewalker <steve.walker@utoronto.ca>
2021-08-02 19:45:07 -0400

    small doc edit in utilities

--- R/iidda/R/utilities.R
+++ R/iidda/R/utilities.R
@@ -1,10 +1,28 @@
-#' Remove Trailing Slash
+#' Lightweight Templating
 #'
-#' @param x Character vector with paths
-#' @return Character vector without trailing slash
+#' Version of the \code{sprintf} base R function that
+#' adds basic templating -- https://stackoverflow.com/a/55423080/2047693
+#'
+#' @param template
+#' @param ... Named arguments with strings that fill template variables
+#' of the same name between \code{%\{} and \code{\}s}
+#' @examples
+#' sprintf_named("You might like to download datasets from %{repo}s.", repo = "IIDDA")
 #' @export
-rm_trailing_slash = function(x) sub('/$', '', x)
+sprintf_named <- function(template, ...) {
+  args <- list(...)
+  argn <- names(args)
+  if(is.null(argn)) return(sprintf(template, ...))
 
+  for(i in seq_along(args)) {
+    if(argn[i] == "") next;
+    template <- gsub(sprintf("%%{%s}", argn[i]),
+                     sprintf("%%%d$", i),
+                     template, fixed = TRUE)
+  }
+
+  do.call(sprintf, append(args, template, 0))
+}
 
 #' Extract Substring Between Parentheses
 #'
@@ -50,27 +68,9 @@ remove_between_paren = function(x, left = "\\(", right = "\\)") {
   sub(pattern, '', x)
 }
 
-#' Lightweight Templating
+#' Remove Trailing Slash
 #'
-#' https://stackoverflow.com/a/55423080/2047693
-#'
-#' @param template
-#' @param ... Named arguments with strings that fill template variables
-#' of the same name between \code{%\{} and \code{\}s}
-#' @examples
-#' sprintf_named("You might like to download datasets from %{repo}s.", repo = "IIDDA")
+#' @param x Character vector with paths
+#' @return Character vector without trailing slash
 #' @export
-sprintf_named <- function(template, ...) {
-  args <- list(...)
-  argn <- names(args)
-  if(is.null(argn)) return(sprintf(template, ...))
-
-  for(i in seq_along(args)) {
-    if(argn[i] == "") next;
-    template <- gsub(sprintf("%%{%s}", argn[i]),
-                     sprintf("%%%d$", i),
-                     template, fixed = TRUE)
-  }
-
-  do.call(sprintf, append(args, template, 0))
-}
+rm_trailing_slash = function(x) sub('/$', '', x)
