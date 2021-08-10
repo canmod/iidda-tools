ee84b34f38b2a314ba39ebbbcbb49758356f510a
stevencarlislewalker <steve.walker@utoronto.ca>
2021-08-10 16:26:33 -0400

    more robust parenthesis string processing, and push new man pages

--- R/iidda/R/utilities.R
+++ R/iidda/R/utilities.R
@@ -3,16 +3,47 @@
 #' Version of the \code{sprintf} base R function that
 #' adds basic templating -- https://stackoverflow.com/a/55423080/2047693
 #'
+#' Because this is based on the sprintf function, use \code{%%} when you
+#' would like a single \code{%} to appear in the template. However, when
+#' supplying a single \code{%} to a named argument will result in a single
+#' \code{%} in the output.
+#'
+#' You can use syntactically invalid names for arguments by enclosing them
+#' in backticks in the argument list, but not in the template.
+#'
 #' @param template
 #' @param ... Named arguments with strings that fill template variables
 #' of the same name between \code{%\{} and \code{\}s}
+#' @param .check Should the consistency between the arguments and the template be checked?
 #' @examples
 #' sprintf_named("You might like to download datasets from %{repo}s.", repo = "IIDDA")
 #' @export
-sprintf_named <- function(template, ...) {
+sprintf_named <- function(template, ..., .check = TRUE) {
   args <- list(...)
   argn <- names(args)
-  if(is.null(argn)) return(sprintf(template, ...))
+  if(.check) {
+    names_in_template = extract_all_between_paren(
+      template, left = '%\\{', right = '\\}s',
+      contents_pattern = '[^%]*')
+  } else {
+    # if no checks, assume no names in template
+    names_in_template = character(0)
+  }
+  if(is.null(argn)) {
+    if(length(names_in_template) != 0L) {
+      stop("There are items in the template, but none were specified as arguments.")
+    } else {
+      return(sprintf(template, ...))
+    }
+  }
+  if(.check) {
+    arg_check_pattern = paste0("%\\{", argn, '\\}s')
+    if(!all(sapply(arg_check_pattern, grepl, template))) {
+      stop("Not all items specified as arguments are in the template.")
+    } else if(!all(names_in_template %in% argn)){
+      stop("Not all items in the template were specified as arguments.")
+    }
+  }
 
   for(i in seq_along(args)) {
     if(argn[i] == "") next;
@@ -26,9 +57,15 @@ sprintf_named <- function(template, ...) {
 
 #' Extract Substring Between Parentheses
 #'
+#' Note that unless you specify an appropriate \code{contents_pattern}
+#' \code{extract_between_paren} will not work as you probably expect
+#' if there are multiple sets of parentheses. You can use exclusion patterns
+#' to make this work better (e.g. \code{content_pattern = '[^)]*'}).
+#'
 #' @param x Character vector
 #' @param left Left parenthetical string
 #' @param right Right parenthetical string
+#' @param contents_pattern Regex pattern for the contents between parentheses
 #' @return Character vector with NA's for elements in \code{x} that
 #' do not have parentheses and the substring between the first matching
 #' parentheses.
@@ -39,8 +76,10 @@ sprintf_named <- function(template, ...) {
 extract_between_paren = function(x, left = "\\(", right = "\\)",
                                  contents_pattern = '.*') {
   pattern = sprintf_named(
+    #"(?<=%{left}s)[^][]*(?=%{right}s)"
     "(?<=%{left}s)%{contents_pattern}s(?=%{right}s)",
-    left = left, right = right, contents_pattern = contents_pattern)
+    left = left, right = right, contents_pattern = contents_pattern,
+    .check = FALSE) # checking creates circular call graph
   (x
     %>% regexec(pattern = pattern, perl = TRUE)
     %>% regmatches(x = x)
@@ -62,11 +101,31 @@ extract_between_paren = function(x, left = "\\(", right = "\\)",
 #' x = c("-", "", NA, "1", "3", "1 (Alta.)", "(Sask) 20")
 #' remove_between_paren(x)
 #' @export
-remove_between_paren = function(x, left = "\\(", right = "\\)") {
+remove_between_paren = function(x, left = "\\(", right = "\\)",
+                                contents_pattern = '.*') {
   pattern = sprintf_named(
-    "%{left}s.*%{right}s",
-    left = left, right = right)
+    "%{left}s%{contents_pattern}s%{right}s",
+    left = left, right = right,
+    contents_pattern = contents_pattern,
+    .check = FALSE) # checking creates circular call-graph
   sub(pattern, '', x)
+}
+
+#' @rdname extract_between_paren
+#' @param max_iters maximum number of items to return
+#' @export
+extract_all_between_paren = function(x, left = "\\(", right = "\\)",
+                                     contents_pattern = '.*',
+                                     max_iters = 100) {
+  stopifnot(length(x) == 1L)
+  output = c()
+  for(i in 1:max_iters) {
+    output = append(output, extract_between_paren(x, left, right, contents_pattern))
+    y = remove_between_paren(x, left, right, contents_pattern)
+    if(x == y) {break}
+    else {x = y}
+  }
+  return(output[!is.na(output)])
 }
 
 #' Remove Trailing Slash
