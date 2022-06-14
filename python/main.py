7aa5cd070b0221ceabfc7f2204cee57ce8b2131c
ronaldLjin <ronyjin123@gmail.com>
2022-06-14 11:34:26 -0600

    Changed names and titles.

--- python/main.py
+++ python/main.py
@@ -7,7 +7,7 @@ from jq import jq
 import re
 nest_asyncio.apply()
 
-app = FastAPI(swagger_ui_parameters={"defaultModelsExpandDepth": -1})
+app = FastAPI(title="IIDDA API", swagger_ui_parameters={"defaultModelsExpandDepth": -1})
 
 def generate_filters():
     dataset_list = get_dataset_list(all_metadata=True,clear_cache=False)
@@ -16,7 +16,7 @@ def generate_filters():
         data[x] = "." + data[x]
     return data
 
-@app.get("/datasets")
+@app.get("/dataset_metadata")
 async def datasets(all_metadata: bool = False, key: str = Query("", enum=generate_filters()),value: str ="",jq_query: str = ""):
     if (key == "" or value == "") and jq_query == "":
         return get_dataset_list(all_metadata,clear_cache=False)
@@ -32,8 +32,8 @@ async def datasets(all_metadata: bool = False, key: str = Query("", enum=generat
         else:
             return jq(f'map_values(select(. != "No metadata.") | select({keys[0]} != null) | select({keys[0]} | if type == "array" then (.[] | contains("{value}")) else contains("{value}") end))').transform(data)
 
-@app.get("/datasets/{dataset_name}")
-async def dataset_name(dataset_name: str,response_type: str = Query("dataset_download", enum=sorted(["dataset_download", "pipeline_dependencies", "github_url", "raw_csv", "metadata", "csv_dialect", "data_dictionary"])), version: str = "latest", metadata: bool =False):
+@app.get("/dataset/{dataset_name}")
+async def dataset(dataset_name: str,response_type: str = Query("dataset_download", enum=sorted(["dataset_download", "pipeline_dependencies", "github_url", "raw_csv", "metadata", "csv_dialect", "data_dictionary"])), version: str = "latest", metadata: bool =False):
     if response_type == "pipeline_dependencies":
         return get_pipeline_dependencies(dataset_name,version)
     else:
