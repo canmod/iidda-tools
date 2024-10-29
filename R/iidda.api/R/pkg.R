52e6077d4e2674348a7ba6907e798823de63c4cf
stevencarlislewalker <stevencarlislewalker@gmail.com>
2024-10-29 04:30:03 -0400

    organize files

---
+++ R/iidda.api/R/pkg.R
@@ -0,0 +1,20 @@
+#' \pkg{iidda.api}
+#'
+#' R wrapper for the IIDDA API.
+#'
+#' IIDDA is the International Infectious Disease Data Archive. This archive
+#' has an [API](https://math.mcmaster.ca/iidda/api/docs)
+#' (Application Programming Interface)
+#' for accessing data and potentially for building applications.
+#' This R package provides a simple wrapper to this API so that
+#' the datasets are returned as data frames.
+#'
+#' Useful links for people who just want to get data.
+#' * `?featured`
+#' * `vignette("Quickstart")`
+#' * `vignette("Provenance")`
+#'
+#' More advanced users might be interested in the lower-level wrapper
+#' of the API operations here: `?ops`.
+#'
+"_PACKAGE"
